bc64bc92c19dcdee87cd7a68a72495cfd1214541
root <root@raspberrypi.(none)>
2013-12-20 01:35:33 +0000

    error fixed and pd repository has been merged I think

--- pdSetup.sh
+++ pdSetup.sh
@@ -8,17 +8,15 @@
 cd /home/pi/
 sudo git clone http://github.com/jhjlim/pd
 cd /home/pi/pd/
-sudo git checkout working
+sudo cp Pd-0.43.3-extended-20121004.deb /home/pi/
+cd /home/pi/
+sudo dpkg -i Pd-0.43.3-extended-20121004.deb
+sudo aptget -f install
+sudo chmod 4755 /usr/bin/pd-extended
 cd /boot/
 sudo rm config.txt
 cd /home/pi/pd
 sudo cp config.txt /boot/
 cd /home/pi/pd
 sudo cp bigText.sh /bin/
-cd /home/pi/pd
-sudo cp /Pd-0.43.3-extended-20121004.deb /home/pi/
-cd /home/pi/
-sudo dpkg -i Pd-0.43.3-extended-20121004.deb
-sudo apt-get -f install
-sudo chmod 4755 /usr/bin/pd-extended
 
