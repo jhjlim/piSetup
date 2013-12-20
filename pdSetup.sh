2f89256940f5f264d2ef39cb41e62246386a6a2c
root <root@raspberrypi.(none)>
2013-12-20 01:13:34 +0000

    uses pd installer from git

--- pdSetup.sh
+++ pdSetup.sh
@@ -1,8 +1,10 @@
-cd /home/pi/
-sudo wget https://puredata.info/downloads/pd-extended-0-43-3-on-raspberry-pi-raspbian-wheezy-armhf/releases/1.0/Pd-0.43.3-extended-20121004.deb
-sudo dpkg -i Pd-0.43.3-extended-20121004.deb
-sudo apt-get -f install
-sudo chmod 4755 /usr/bin/pd-extended
+#cd /home/pi/
+#sudo wget https://puredata.info/downloads/pd-extended-0-43-3-on-raspberry-pi-raspbian-wheezy-armhf/releases/1.0/Pd-0.43.3-extended-20121004.deb
+#cd /home/pi/pd
+#sudo cp /Pd-0.43.3-extended-20121004.deb /home/pi/
+#sudo dpkg -i Pd-0.43.3-extended-20121004.deb
+#sudo apt-get -f install
+#sudo chmod 4755 /usr/bin/pd-extended
 cd /home/pi/
 sudo git clone http://github.com/jhjlim/pd
 cd /home/pi/pd/
@@ -13,5 +15,10 @@ cd /home/pi/pd
 sudo cp config.txt /boot/
 cd /home/pi/pd
 sudo cp bigText.sh /bin/
-
+cd /home/pi/pd
+sudo cp /Pd-0.43.3-extended-20121004.deb /home/pi/
+cd /home/pi/
+sudo dpkg -i Pd-0.43.3-extended-20121004.deb
+sudo apt-get -f install
+sudo chmod 4755 /usr/bin/pd-extended
 
